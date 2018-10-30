e42aeab4206c9d9394a9ac2bca9f9879bd0afc8b
Ubuntu <ubuntu@ubularavel.lxd>
2018-10-30 17:01:04 +0000

    primer prueba1

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
-//$server="localhost";
-$server="mysql.hostinger.com.ar";	
-$user="u410159394_agen";
-$bd="u410159394_agen";
+$server="localhost";
+//$server="mysql.hostinger.com.ar";
+$user="u410159394_reser";
+$bd="u410159394_reser";
 $clave="sistemas";
-	
-$mysqli=new mysqli($server,$user,$clave,$bd);	
-	
+
+$mysqli=new mysqli($server,$user,$clave,$bd);
+
 //$mysqli=new mysqli("127.0.0.1","sistema","sistema","sistema"); //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	
+
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();

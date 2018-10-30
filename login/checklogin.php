87c266c9245a2c2836c6f692c28cb3b432b31c99
marcelox <marcelox@marcelox-H61M>
2018-10-30 15:22:17 -0300

    hoy

--- login/checklogin.php
+++ login/checklogin.php
@@ -8,7 +8,7 @@ include_once "../conexion.php";
 
 
 if ($mysqli->connect_error) {
- die("La conexion falló: " . $mysqli->connect_error);
+  die("La conexion falló: " . $mysqli->connect_error);
 }
 
 $username = $_POST['username'];
@@ -19,30 +19,29 @@ $sql = "SELECT * FROM usuario WHERE nombre = '$username' and clave = '$password'
 $result = $mysqli->query($sql);
 //echo '<div class="error">'.$result->num_rows.'</div>';
 
-if ($result->num_rows > 0) {     
- }
- $row = $result->fetch_array(MYSQLI_ASSOC);
- //echo '<div class="error">'.$password.' '.$row['clave'].'</div>';
- if ($password==$row['clave']) { 
- 
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-    $_SESSION['categoria'] = $row['category_codigo'];
-    $_SESSION['id_usuario']=$row['id_usuario'];
-    $_SESSION['start'] = time();
-    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
-    //$_SESSION['expire'] = $_SESSION['start'] + (1 * 20);
+if ($result->num_rows > 0) {
+}
+$row = $result->fetch_array(MYSQLI_ASSOC);
+//echo '<div class="error">'.$password.' '.$row['clave'].'</div>';
+if ($password==$row['clave']) {
 
-    echo "Bienvenido! " . $_SESSION['username'];
-    //echo "<br><br><a href=control.php>Panel de Control</a>";
-    header('Location: ../menu/index.php');
- 
+  $_SESSION['loggedin'] = true;
+  $_SESSION['username'] = $username;
+  $_SESSION['categoria'] = $row['category_codigo'];
+  $_SESSION['id_usuario']=$row['id_usuario'];
+  $_SESSION['start'] = time();
+  $_SESSION['expire'] = $_SESSION['start'] + (105 * 60);
 
- } else { 
-   echo "Username o Password estan incorrectos.";
 
-   echo "<br><a href='login.html'>Volver a Intentarlo</a>";
- }
- mysqli_close($mysqli); 
- ?>
- 
+  echo "Bienvenido! " . $_SESSION['username'];
+  //echo "<br><br><a href=control.php>Panel de Control</a>";
+  header('Location: ../menu/index.php');
+
+
+} else {
+  echo "Username o Password estan incorrectos.";
+
+  echo "<br><a href='login.html'>Volver a Intentarlo</a>";
+}
+mysqli_close($mysqli);
+?>
--- menu/inicio.php
+++ menu/inicio.php
@@ -1,26 +1,21 @@
-<?php 
+<?php
 include ('../login/control.php');
-?> 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="menu3.css" />
+	<!-- <link rel="stylesheet" type="text/css" href="menu3.css" /> -->
+	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<title>inicio</title>
 </head>
 <body>
-	
-<!--<div class="presentacion" >
-	
-	&nbsp;<img id="mostrar_img" src="imagenes/tux-graduate.png" alt="TuxGraduate" width="400" onclick="cambiar()"></img>
 
-</div>-->
-<img id="mostrar_img" src="imagenes/tux-graduate.png" alt="TuxGraduate" width="400" onclick="cambiar()"></img>
+<div class="container-fluid" >
 
-<br><br>
-<center><input type="button" onclick=" location.href='charts.php' " value="Ver estadisticas" name="boton" /></center>
-<br><br>
-<center><input type="button" onclick=" location.href='../reloj/index.html' " value="Reloj JavaScript" name="botonR" /></center>
+	<img id="mostrar_img" src="imagenes/tux-graduate.png" alt="TuxGraduate" width="400" onclick="cambiar()"></img>
+
+</div>
 
 </body>
 
@@ -34,7 +29,7 @@ function cambiar() {
 	document.getElementById("mostrar_img").style.width="400px";
 	i=2;
 	}
-	else 
+	else
 	{
 	document.images["mostrar_img"].src="imagenes/der.png";
 	document.getElementById("mostrar_img").style.width="900px";

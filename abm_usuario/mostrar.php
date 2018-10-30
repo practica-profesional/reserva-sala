4e44ed33e9578abea008a855f42b56f6f081340f
marcelofajardo <marcelo.fajardo@gmail.com>
2018-10-30 20:41:40 -0300

    boostrap 4

--- abm_usuario/mostrar.php
+++ abm_usuario/mostrar.php
@@ -4,12 +4,12 @@
 	session_start();
 	$categoria=$_SESSION['categoria'];
 	$username=$_SESSION['username'];
-	
+
 	if((isset($_POST['busqueda']) && $_POST['busqueda']<>'')or(isset($_POST['busquedaXcategoria']) && $_POST['busquedaXcategoria']<>'')){
 		$nom=$_POST['busqueda'];
 		$cat=$_POST['busquedaXcategoria'];
 
-	/*$query="SELECT id_usuario, nombre, email, category_codigo FROM usuario 
+	/*$query="SELECT id_usuario, nombre, email, category_codigo FROM usuario
 			where nombre like '$nom%' or category_codigo = '$cat'";*/
 	$query="SELECT id_usuario, u.nombre as nombre, email, c.nombre as categoria FROM usuario u, category c
 			WHERE c.codigo = u.category_codigo AND (u.nombre LIKE  '$nom%' and c.nombre like '$cat%')";
@@ -18,73 +18,86 @@
 	else{
 	$query="SELECT id_usuario, u.nombre as nombre, email, c.nombre as categoria FROM usuario u, category c
 			WHERE c.codigo = u.category_codigo";
-	} 
-	echo $query;
+	}
+	//echo $query;
 	$resultado=$mysqli->query($query);
 
 ?>
 
 <html>
 	<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Usuarios</title>
-	<link rel="stylesheet" type="text/css" href="../tablas.css" media="screen">
+	<!-- <link rel="stylesheet" type="text/css" href="../tablas.css" media="screen"> -->
+	<link rel="stylesheet" type="text/css" href="../bootstrap/sketchy/bootstrap.min.css">
 	</head>
 	<body>
-		
+
 		<center><h1>Usuarios</h1></center>
 
-		<div style="float: left;">
-			<input type="button" onclick=" location.href='nuevo.php' " value="Nuevo usuario" name="boton" />
-		</div>
-		<div id="filtros" style="float: right;">
-			<form action="mostrar.php" method="post">
-			<p> Busqueda por nombre: 
-			<input type="text" name="busqueda">
-			y/o categoria: 
-			<input type="text" name="busquedaXcategoria">
-			<button type="submit">Filtrar</button></p>
+		<div class="container float-none">
+
+		<!-- </div>
+		<div id="filtros" style="float: right;"> -->
+			<form class="form" action="mostrar.php" method="post">
+				<div class="form-group float-left">
+						<input type="button" class="btn btn-primary"
+								onclick=" location.href='nuevo.php' "
+								value="Nuevo usuario" name="boton" />
+				</div>
+				<div class="form-group float-right">
+						<p> Busqueda por nombre:
+						<input type="text" name="busqueda">
+						y/o categoria:
+						<input type="text" name="busquedaXcategoria">
+						<button type="submit" class="btn btn-primary">Filtrar</button></p>
+				</div>
 			</form>
 		</div>
-		<p></p>
-		<div class="group">
-		<table class="table1" border=1 width="100%" cellspacing=0>
-			<thead>
-				<tr>
-					<th><b>Nombre</b></th>
-					<th><b>Email</b></th>
-					<th><b>Categoria</b></th>
-					<th><b>Modificar</b></th>
-					<th><b>Eliminar</b></th>
-				</tr>
-				<tbody>
-					<?php while($row=$resultado->fetch_assoc()){ ?>
+
+		<div class="container">
+			<div class="table-hover table-responsive">
+				<table class="table table-striped">
+					<thead class="thead-dark">
 						<tr>
-							<td><?php echo $row['nombre'];?>
-							</td>
-							<td>
-								<?php echo $row['email'];?>
-							</td>
-							<td>
-								<?php echo $row['categoria'];?>
-							</td>							
-							<td>
-								<input type="button" onclick=" location.href='modificar.php?id=<?php echo $row['id_usuario'];?>' " value="Modificar" name="botonM" />
-							</td>
-							<td>
-								
-								<?php if ( $row['nombre'] != $username ){ ?>
-
-										 	<input type="button" onclick=" location.href='pre_eliminar.php?id=<?php echo $row['id_usuario'];?>' " value="Eliminar" name="botonE" />
-
-												
-								<?php } ?>
-							
-							</td>
+							<th ><b>Nombre</b></th>
+							<th><b>Email</b></th>
+							<th><b>Categoria</b></th>
+							<th><b>Modificar</b></th>
+							<th><b>Eliminar</b></th>
 						</tr>
-					<?php } ?>
-				</tbody>
-		</table>
+						<tbody>
+							<?php while($row=$resultado->fetch_assoc()){ ?>
+								<tr>
+									<td><?php echo $row['nombre'];?>
+									</td>
+									<td>
+										<?php echo $row['email'];?>
+									</td>
+									<td>
+										<?php echo $row['categoria'];?>
+									</td>
+									<td>
+										<input type="button" class="btn btn-secondary" onclick=" location.href='modificar.php?id=<?php echo $row['id_usuario'];?>' " value="Modificar" name="botonM" />
+									</td>
+									<td>
+
+										<?php if ( $row['nombre'] != $username ){ ?>
+
+												 	<input type="button" class="btn btn-secondary" onclick=" location.href='pre_eliminar.php?id=<?php echo $row['id_usuario'];?>' " value="Eliminar" name="botonE" />
+
+
+										<?php } ?>
+
+									</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+				</table>
+			</div>
 		</div>
 	</body>
-</html>	
-	
+	<script src="../boostrap/js/bootstrap.min.js"></script>
+</html>
--- menu/index.php
+++ menu/index.php
@@ -1,28 +1,30 @@
-<?php 
-session_start(); 
-?> 
+<?php
+session_start();
+?>
 
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" type="text/css" href="menu3.css" />
+	<link rel="stylesheet" type="text/css" href="../bootstrap/sketchy/bootstrap.min.css">
 	<link rel="stylesheet" href="fonts/style.css">
-	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script> 
+	<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
 	<script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
 </head>
 
 <body onload="ocultar()">
 		<header>
-		<nav>
+		<nav role="navigation">
+
 			<ul>
 				<li onclick="mostrar()"><a href="inicio.php" target="contenido"><span class="inicio"><i class="icon icon-home"></i></span>Inicio</a></li>
 				<li id="users"><a href="../abm_usuario/mostrar.php" target="contenido"><span class="usuarios"><i class="icon icon-key"></i></span>ABM Usuarios</a>
 				</li>
 				<li id="Personas"><a href="../abm_personas/personas.php" target="contenido"><span class="personas"><i class="icon icon-pacman"></i></span>ABM Personas</a></li>
 				<li id="Citas"><a href="#"><span class="citas"><i class="icon icon-pencil"></i></span>ABM Citas y Notas</a>
-					<ul>
+					<ul class="dropdown-menu">
 						<li><a href="../abm_citas/Citas.php" target="contenido">Citas</a></li>
 						<li><a href="../abm_notas/notas.php" target="contenido">Notas</a></li>
 					</ul>
@@ -30,6 +32,7 @@ session_start();
 				</li>
 				<li onclick="BotonSalir()"><a id="Salir" href="#" ><span class="salir"><i class="icon icon-rocket"></i></span>Logout</a></li>
 			</ul>
+
 		</nav>
 	</header>
 <!--<div>
@@ -40,19 +43,22 @@ session_start();
 </div>-->
 
 	<div id="global">
-		<iframe name="contenido" id="external" style="width:100%;height:100%;border: 0;margin: auto;padding: 0px auto;" src="inicio.php"></iframe>
+		<iframe name="contenido" id="external"
+						style="width:100%;height:100%;border: 0;margin: auto;padding: 0px auto;"
+						src="inicio.php">
+		</iframe>
 	</div>
 
 
 </body>
 
 
-<script language="javascript"> 
+<script language="javascript">
 window.onload = function(){
 	//ocultar();
 	mostrar();
-} 
-</script> 
+}
+</script>
 <script type="text/javascript">
 //$("#users").hide("slow");
 //document.getElementById('users').style.display = 'none';
@@ -62,7 +68,7 @@ window.onload = function(){
 </script>
 <!--<script type="text/javascript">
 
- $(document).ready(function(){ 
+ $(document).ready(function(){
    $("#Salir").on("click",function(){
       $("#users").toggle("slow","linear");
    });
@@ -72,7 +78,7 @@ window.onload = function(){
 <script type="text/javascript">
 function mostrar(){
 	var silogin=false;
-	
+
 		var username='<?php echo $_SESSION['username']; ?>';
 		var categoria='<?php echo $_SESSION['categoria']; ?>';
 		var silogin='<?php echo $_SESSION['loggedin']; ?>';

6dc0ed1b191e6f86932e3b95b19b339366a953c1
marcelox <marcelox@marcelox-H61M>
2018-11-02 09:22:44 -0300

    calendario prueba

---
+++ fullcalendar-jQuery-PHP/db.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$usuariodb = "u410159394_reser";
+$contrasenadb = "sistemas";
+$dbname = "u410159394_reser";
+	
+// Generar conexion con el servidor MySQl
+$conexion = mysqli_connect($hostname, $usuariodb, $contrasenadb, $dbname);

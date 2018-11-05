3a78a87fd2c2f219bf357631aeef7d7d3444690f
marcelofajardo <marcelo.fajardo@gmail.com>
2018-11-05 00:35:10 -0300

    combos

--- calendario/index.php
+++ calendario/index.php
@@ -21,6 +21,19 @@ body {
     font-size: 14px;
 }
 </style>
+<style type="text/css">
+	li.dropdown-submenu:hover ul{
+	display: block;
+	left: 100%;
+	}
+	.dropdown-submenu{
+		position:relative;
+	}
+
+	.dropdown-submenu:hover .dropdown-menu{
+		top:0;
+	}
+</style>
 		<script src='js/jquery.min.js'></script>
 		<script src='js/bootstrap.min.js'></script>
 		<script src='js/moment.min.js'></script>
@@ -73,6 +86,20 @@ body {
 					]
 				});
 			});
+		</script>
+		<script>
+		$(function(){
+		    $(".dropdown-menu li a").click(function(){
+		        var item = $(this);
+		        var id = item.parent().parent().attr("aria-labelledby");
+							//alert(id);
+
+
+		        $("#"+id+":first-child").text($(this).text());
+		        $("#"+id+":first-child").val($(this).text());
+						document.getElementById("text-" + id).value = $(this).text();
+		   });
+		});
 
 		</script>
 
@@ -91,36 +118,127 @@ body {
 				<div class="panel-heading">Elija Sala</div>
 		    <div class="panel-body">
 
+<p></p>
+
+<!--<div class="btn-group">
+    <a class="btn btn-default dropdown-toggle btn-blog " data-toggle="dropdown" href="#" id="dropdownMenu1" style="width:200px;"><span class="selection pull-left">Select an option </span>
+      <span class="pull-right glyphiconglyphicon-chevron-down caret" style="float:right;margin-top:10px;"></span></a>
+
+     <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+       <li><a href="#" class="" data-value=1><p> HER Can you write extra text or <b>HTLM</b></p> <h4> <span class="glyphicon glyphicon-plane"></span>  <span> Your Option 1</span> </h4></a>  </li>
+       <li><a href="#" class="" data-value=2><p> HER Can you write extra text or <i>HTLM</i> or some long long long long long long long long long long text </p><h4> <span class="glyphicon glyphicon-briefcase"></span> <span>Your Option 2</span>  </h4></a>
+      </li>
+      <li class="divider"></li>
+   <li><a href="#" class="" data-value=3><p> HER Can you write extra text or <b>HTLM</b> or some </p><h4> <span class="glyphicon glyphicon-heart text-danger"></span> <span>Your Option 3</span>  </h4></a>
+      </li>
+    </ul>
+  </div>
+  <input type="text" id="vl" /> -->
+	<form class="form-horizontal" method="POST" action="eliminar_evento.php">
 
 					<div class="dropdown">
-				    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Salas por Zonas
-				    <span class="caret"></span></button>
-				    <ul class="dropdown-menu">
+				    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu2"
+						data-toggle="dropdown">Salas por Zonas
+				    <span class="selection"></span><span class="caret"></span></button>
+				    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu2">
 				      <li class="dropdown-header">Zona Niza</li>
-				      <li><a href="#">Sala Reuniones</a></li>
-				      <li><a href="#">Sala videconferencia</a></li>
-				      <li><a href="#">Sla 3</a></li>
+				      <li><a href="#">Sala Reuniones Niza</a></li>
+				      <li><a href="#">Sala videconferencia Niza</a></li>
+				      <li><a href="#">Sala 3 Niza</a></li>
 				      <li class="divider"></li>
 				      <li class="dropdown-header">Zona Deheza</li>
-				      <li><a href="#">Sala Unica</a></li>
+				      <li><a href="#">Sala Unica Deheza</a></li>
 				    </ul>
 				  </div>
+
 					<p></p>
 					<div class="text">
 						<label for="quien">Quien solicita la sala, por favor ingrese su email</label>
-						<input type="text" name="quie" value="">
+						<input type="text" name="quien" value="">
 					</div>
 					<p></p>
 
+
+
+					<p></p>
+
 					<div class="dropdown">
-						<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">cuantas personas
+						<button class="btn btn-default dropdown-toggle" type="button" id="cantpers" data-toggle="dropdown">cuantas personas
 						<span class="caret"></span></button>
-						<ul class="dropdown-menu">
+						<ul class="dropdown-menu" aria-labelledby="cantpers">
 							<li><a href="#">1 -5</a></li>
 							<li><a href="#">5 - 20</a></li>
 							<li><a href="#">mas de 20</a></li>
 						</ul>
 					</div>
+
+					<p></p>
+
+					<!--<div class="dropdown">
+						<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Hora de inicio
+						<span class="caret"></span></button>
+						<ul class="dropdown-menu">
+							<li><a href="#">8 am</a></li>
+							<li><a href="#">10 am</a></li>
+							<li><a href="#">12 am</a></li>
+						</ul>
+					</div>
+
+					<p></p>
+
+					<div class="dropdown">
+						<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">Hora de fin
+						<span class="caret"></span></button>
+						<ul class="dropdown-menu">
+							<li><a href="#">8 am</a></li>
+							<li><a href="#">10 am</a></li>
+							<li><a href="#">12 am</a></li>
+						</ul>
+					</div>
+
+					<p></p>
+
+					<div class="dropdown">
+						<button class="btn btn-default dropdown-toggle" type="button"
+						 data-toggle="dropdown">Tipo de uso
+						<span class="caret"></span></button>
+						<ul class="dropdown-menu" aria-labelledby="tipouso" id="tipouso">
+							<li><a href="#">videconferencia</a></li>
+							<li><a href="#">Meeting</a></li>
+							<li><a href="#">Laboral</a></li>
+						</ul>
+					</div>
+-->
+					<p></p>
+
+
+					<p></p>
+
+					<button class="btn btn-default btn-success" type="button" name="button"
+					data-toggle="modal" data-target="#reservaModal">Enviar reserva</button>
+</form>
+
+
+					<div class="modal fade" id="reservaModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+								aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+									<h4 class="modal-title" id="myModalLabel">datos a enviar para reserva de sala</h4>
+								</div>
+								<div class="modal-body">
+									<input type="text" name="prueba" id="text-dropdownMenu2" value="">
+									Texto de la reserva
+									Tipo de uso:  tipouso.text
+								</div>
+							</div>
+						</div>
+					</div>
+
+
 
 				</div>
 		  </div>
